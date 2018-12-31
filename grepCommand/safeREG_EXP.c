d5f9179412b8ab9c1a375a24b286dadb980b81b0
Tarun Gopalkrishna <tarungopalkrishna@gmail.com>
2018-12-31 18:50:30 +0530

    Cleared a bug with malloc and add the srealloc() function.

---
+++ grepCommand/safeREG_EXP.c
@@ -0,0 +1,292 @@
+#include <stdio.h>
+//#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "../safeMemoryAllocation/salloc.h"
+#define bufferSize 2000
+#define maxArguments 5
+/* 
+	The case 'r' is a new addition which searches the pattern string in the reverse manner
+	I'm sorry if there is already an option with that functionality
+*/
+/*
+	Return values:
+	0 -> successful run.
+	1 -> Memeory was not allocated properly.
+	2 -> File failed to open.
+*/
+int globalLineCounter = 0,globalOccurencesCounter = 0;
+void charcmpi(char a,char b)
+{
+
+}
+int optionContains(char a,char *options)
+{
+	//int flag = 0;
+	for(int i=0;i<strlen(options);i++){
+		if(a == *(options + i)){
+			return 1;
+		}
+	}
+	return 0;
+}
+int searchPattern(char *pattern,char *buffer)
+{
+	printf(" Search PATTERN -------------------------------------------------------------\n");
+	int flag = 0;
+	static int lineCounter = 0;
+	//printf(" The pattern to be searched is: %s\n", pattern);
+	//printf(" The buffer is: %s", buffer);
+	lineCounter++;
+	for(int i=0;i < strlen(buffer);i++){
+		for (int j = 0; j < strlen(pattern); j++){
+			//printf(" Line number: %d i==%d j==%d %c==%c\n", lineCounter,i,j,*(buffer+i),*(pattern+j));
+			if(*(buffer+i)==*(pattern+j)){
+				//printf(" It has matched. %c == %c YES!\n",*(buffer+i),*(pattern+j));
+				i++;
+				if(j == strlen(pattern) - 1){
+					if(flag == 0){
+						globalLineCounter++;
+						printf(" %d -> %s\n", lineCounter,buffer);
+					}
+					globalOccurencesCounter++;
+					flag = 1;
+				}
+			}
+			else{
+				break;
+			}
+		}
+	}
+	return flag;
+}
+int openFileToSearch(char *pattern,char *fileName,char *option)
+{
+	int flag = 0;
+	char *buffer = NULL;
+	FILE *file;
+	file = fopen(fileName,"r");
+	if(file == NULL){
+		printf(" The file %s was not able to open.\n", fileName);
+		return -1;
+	}
+	else if(option == NULL){
+		buffer = (char*)smalloc(bufferSize);
+		while(fgets(buffer,bufferSize,file) != NULL){
+			//printf(" The lenght of the characters read is: %ld\n", strlen(buffer));
+			if(strlen(buffer) == bufferSize - 1){
+				printf(" The prgram will not work for this line.\n");
+				printf(" Enter 'y' to continue......");
+				scanf("%s",buffer);
+				continue;
+			}
+			*(buffer + strlen(buffer) - 1) = '\0'; /* To remove the newline character at the end of the line*/
+			if(searchPattern(pattern,buffer) == 1){
+				flag = 1;
+			}
+		}
+	}
+	else{
+		buffer = (char*)smalloc(bufferSize);
+		//printf("->%s<-\n", option);
+		while(fgets(buffer,bufferSize,file) != NULL){
+			if(strlen(buffer) == bufferSize - 1){
+				printf(" The prgram will not work for this line.\n");
+				printf(" Enter 'y' to continue......");
+				scanf("%s",buffer);
+				continue;
+	 		}
+			*(buffer + strlen(buffer) - 1) = '\0'; /* To remove the newline character at the end of the line*/
+			if(optionContains('i',option+1) == 1){
+				printf(" Contains i.\n");
+			}
+			if(optionContains('r',option+1) == 1){
+				printf(" Contains r.\n");
+			}
+			if(optionContains('v',option+1) == 1){
+				printf(" Contains v.\n");
+			}
+			if(optionContains('z',option+1) == 1){
+				printf(" Contains 0.\n");
+			}
+			if(optionContains('z',option+1) == 1){
+				printf(" Contains 0.\n");
+			}
+			if(optionContains('z',option+1) == 1){
+				printf(" Contains 0.\n");
+			}
+			/*
+			for(int i=1;i<strlen(option);i++){
+				switch(*(option+i)){
+					case 'v' :	printf(" Case v is working.\n");
+								break;
+					case 'i' :	printf(" Case i is working.\n");
+								break;
+					case 'r' :	printf(" Case r is working.\n");
+								break;
+					default  :	printf(" Okay\n");
+				}
+			}
+			*/
+			break;		
+		}
+	}
+	fclose(file);
+	sfree(buffer);
+	if(flag == 1){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+char *newAllocation(int n,...)
+{
+	if(n>0){
+		int size = 0,count = 1;
+		char *stringParser,*returnString;
+		va_list argument;
+		va_start(argument, n);
+		stringParser 	= va_arg(argument,char*);
+		size 			= size + strlen(stringParser);
+		returnString 	= (char*)smalloc(size);
+		strcpy(returnString,stringParser);
+		/* This part of the code is not tested for multile cases*/
+		getMemoryStatus();
+		while(count < n){
+			printf(" Inside working\n");
+			stringParser 	= va_arg(argument,char*);
+			printf(" stringParser:%s\n", stringParser);
+			size 			= size + strlen(stringParser);
+			printf(" size:%d\n", size);
+			returnString 	= (char*)srealloc(returnString,size);
+			strcat(returnString,stringParser);
+			count++;
+		}
+		getMemoryStatus();
+		/* End of not tested code*/
+		va_end(argument);
+		return returnString;
+	}
+	else{
+		printf(" The first argument must be a value greater than 1.\n");
+		return NULL;
+	}
+}
+void invalidMessage()
+{
+	printf(" Invalid number of arguments passed to sub program.\n");
+	return;
+}
+/*
+void freeMemory(char *one,char *two,char *three,char *four,char *buffer,char *five)
+{
+	printf(" one   : %p\n", one);
+	printf(" two   : %p\n", two);
+	printf(" three : %p\n", three);
+	printf(" four  : %p\n", four);
+	printf(" five  : %p\n", five);
+	printf(" buffer  : %p\n", buffer);
+	if (one != NULL){
+		sfree(one);	
+	}
+	if (two != NULL){
+		sfree(two);
+	}
+	if (three != NULL){
+		sfree(three);
+	}
+	if (four != NULL){
+		sfree(four);
+	}
+	if (five != NULL){
+		sfree(five);
+	}
+	if (buffer != NULL){
+		sfree(buffer);
+	}
+	return;
+}
+*/
+int main(int argc, char const *argv[])
+{
+	if(argc > maxArguments){
+		invalidMessage();
+	}
+	else{
+		int newBytes,switchCase,searchReturnValue;
+		char *fileName=NULL,*currentDirectory=NULL,*pattern=NULL,*completePathName=NULL,*option=NULL;
+		char *buffer = NULL;
+		//switchCase = checkIfHasOption(argc,*argv);
+		buffer = (char*)smalloc(bufferSize);
+		if(buffer == NULL){
+			printf(" Memory was not allocated to the buffer.\n");
+			return 1;
+		}
+		for (int i = 0; i < argc; ++i){
+			//printf("%s\n", argv[i]);
+			strcpy(buffer,argv[i]);
+			//printf("%c\n", *(buffer));
+			if(*(buffer) == '-'){
+				//printf(" This is an option.\n");
+				switchCase = 2;
+				break;
+			}
+			switchCase = 1;
+		}
+		printf(" switchCase is: %d\n", switchCase);
+		switch(switchCase){
+			case 1 :strcpy(buffer,argv[1]);
+					//printf(" Before pattern\n");
+					pattern = newAllocation(1,buffer);
+					//printf(" The pointer of pattern is: %p\n", pattern);
+					//printf(" After pattern\n");
+					strcpy(buffer,argv[2]);
+					//printf(" Before currentDirectory\n");
+					currentDirectory = newAllocation(1,buffer);
+					//printf(" Before currentDirectory_1\n");
+					strcpy(buffer,"/");
+					//printf(" Before currentDirectory_2\n");
+					strcat(buffer,argv[3]);
+					//printf(" Before fileName\n");
+					fileName = newAllocation(1,buffer);
+					newBytes = strlen(currentDirectory) + strlen(fileName);
+					printf(" %s : %s\n", currentDirectory,fileName);
+					completePathName = newAllocation(2,currentDirectory,fileName);
+					printf(" completePathName : %s\n", completePathName);
+					break;
+			case 2 :strcpy(buffer,argv[1]);
+					option = newAllocation(1,buffer);
+					strcpy(buffer,argv[2]);
+					pattern = newAllocation(1,buffer);
+					strcpy(buffer,argv[3]);
+					currentDirectory = newAllocation(1,buffer);
+					strcpy(buffer,"/");
+					strcat(buffer,argv[4]);
+					fileName = newAllocation(1,buffer);
+					newBytes = strlen(currentDirectory) + strlen(fileName);
+					completePathName = newAllocation(2,currentDirectory,fileName);
+					//printf("%s - %s - %s - %s - %s - %d\n", option,pattern,currentDirectory,fileName,completePathName,newBytes);
+					break;
+			default:printf(" Something went horribally wrong!.\n");
+					exit(0);
+		}
+		printf("Send HELP\n");
+		searchReturnValue = openFileToSearch(pattern,completePathName,option);
+		if(searchReturnValue == 0){
+			printf(" Pattern NOT found.\n");
+		}
+		else if(searchReturnValue != 2){
+			printf(" The globalOccurencesCounter value is: %d\n", globalOccurencesCounter);
+			printf(" The globalLineCounter value is: %d\n", globalLineCounter);
+		}
+		else{
+			printf(" File \"%s\" not albe to open.\n", completePathName);
+		}
+		printf(" Number of elemets left before clean: %d\n", getMemoryStatus());
+		freeAllMemeory();
+		printf(" Number of elemets left after clean : %d\n", getMemoryStatus());
+		//freeMemory(fileName,currentDirectory,pattern,completePathName,buffer,option);
+	}
+	return 0;
+}
--- safeMemoryAllocation/salloc.c
+++ safeMemoryAllocation/salloc.c
@@ -16,21 +16,23 @@ struct __memoryTable
 	//int 	memoryId;
 	//char 	*pointerName;
 	void 	*memoryAddress;
-	int 	memorySize;		//Think later if you want to make this pointer variable.
+	unsigned int 	memorySize;		//Think later if you want to make this pointer variable.
 	struct __memoryTable *__leftlink;
 	struct __memoryTable *__rightlink;
 };
 struct __memoryTable *__memoryTableStart__ = NULL,*__memoryTableEnd__ = NULL;
 // ALL THE FUNCTIONS
 /* Functions local to the program which must not be used in a user defined program */
-void *__getMemory(void *pointerAddress,unsigned int size); 		//For the progam should not be used in your program.
-int __pushLinkedList(void *pointerAddress,unsigned int size);	//Push into the linked list.
-int __popLinkedList(void *pointerAddress);						//Pop from the linked list.
+void *__getMemory(void *pointerAddress,unsigned int size);	//For the progam should not be used in your program.
+unsigned int __pushLinkedList(void *pointerAddress,unsigned int size);	//Push into the linked list.
+unsigned int __popLinkedList(void *pointerAddress);	//Pop from the linked list.
+void __updateLinkedList(void *updateAddress,void *newMemoryAddress,unsigned int size); // To update if memory is reallocated
 /* The functions that the programmer can use*/
-void getMemoryStatus();											//Gets the currrent allocated addresses.
-void *smalloc(char *dataType,unsigned int size);				//allocated memory and push into the linked list.
-int sfree(void *pointerAddress);								//free memory and pop it from the linked list.
-int freeAllMemeory();											//free all the allocated memory
+extern unsigned int getMemoryStatus();	//Gets the currrent allocated addresses.
+extern void *smalloc(unsigned int size);	//allocated memory and push into the linked list.
+extern void *srealloc(void *pointerAddress,unsigned int size);		//reallocates a new piece of memory and updates __memoryTable
+extern unsigned int sfree(void *pointerAddress);	//free memory and pop it from the linked list.
+extern unsigned int freeAllMemeory();	//free all the allocated memory
 // FUNCTION DEFINITIONS
 void *__getMemory(void *pointerAddress,unsigned int size)/* Allocated a node for __memoryTable*/
 {
@@ -42,57 +44,70 @@ void *__getMemory(void *pointerAddress,unsigned int size)/* Allocated a node for
 	temp -> __rightlink = NULL;
 	return temp;
 }
-void getMemoryStatus()
+unsigned int getMemoryStatus()
 {
+	unsigned int count = 0;
 	struct __memoryTable *temp;
 	if(__memoryTableStart__ == NULL){
-		printf(" No allocated memory.\n");
+		//printf(" No allocated memory.\n");
+		return 0;
 	}
 	else{
+		printf(" The memory table is:\n");
 		temp = __memoryTableStart__;
 		while(temp != NULL){
+			count++;
 			printf(" Memory Address: %p\n", temp -> memoryAddress);
+			printf(" Memory size   : %d\n", temp -> memorySize	);
 			printf(" __leftlink    : %p\n", temp -> __leftlink);
 			printf(" __rightlink   : %p\n", temp -> __rightlink);
 			temp = temp -> __rightlink;
+			printf("\n");
 		}
+		printf(" End of memory table.\n");
+		return count;
 	}
-	return;
 }
-int __pushLinkedList(void *pointerAddress,unsigned int size)
+unsigned int __pushLinkedList(void *pointerAddress,unsigned int size)
 {
 	struct __memoryTable *temp;
 	temp = __getMemory(pointerAddress,size);
 	if(temp == NULL){
-		printf(" Cannot push to stack due to insufficient memory.\n");
+		//printf(" Cannot push to linked list due to insufficient memory.\n");
 		return 1;
 	}
 	else{
+		//printf(" I - __memoryTableStart__ = %p\n", __memoryTableStart__);
+		//printf(" I - __memoryTableEnd__   = %p\n", __memoryTableEnd__);
+		//printf(" The memory address to be inserted is: %p\n", temp);
 		if(__memoryTableStart__ == NULL){
 			__memoryTableStart__ = temp;
 			__memoryTableEnd__   = temp;
-			getMemoryStatus();
-			return 0;
 		}
 		else{
 			temp -> __leftlink = __memoryTableEnd__;
 			__memoryTableEnd__ -> __rightlink = temp;
-			__memoryTableEnd__ = __memoryTableEnd__ -> __leftlink;
-			getMemoryStatus();
-			return 0;
+			__memoryTableEnd__ = __memoryTableEnd__ -> __rightlink;
 		}
+		//printf(" O - __memoryTableStart__ = %p\n", __memoryTableStart__);
+		//printf(" O - __memoryTableEnd__   = %p\n", __memoryTableEnd__);
+		//getMemoryStatus();
+		return 0;
 	}
 }
-int __popLinkedList(void *pointerAddress)
+unsigned int __popLinkedList(void *pointerAddress)
 {
 	struct __memoryTable *temp,*prev;
 	if(__memoryTableStart__ == NULL){
-		printf(" The memory table is empty.\n");
+		//printf(" The memory table is empty.\n");
 		return 0;
 	}
 	else{
+		//printf(" I - __memoryTableStart__ = %p\n", __memoryTableStart__);
+		//printf(" I - __memoryTableEnd__   = %p\n", __memoryTableEnd__);
 		temp = __memoryTableStart__;
 		if(__memoryTableStart__ == __memoryTableEnd__){
+			//printf(" 1) The memory getting deallocated is: %p\n", temp -> memoryAddress);
 			free(temp);
 			__memoryTableStart__ = NULL;
 			__memoryTableEnd__ = NULL;
@@ -100,89 +115,106 @@ int __popLinkedList(void *pointerAddress)
 		else{
 			while(temp != NULL){
 				if(temp -> memoryAddress == pointerAddress){	// I'm not checking for INVALID address condition.
-					prev = temp -> __leftlink;
-					prev -> __rightlink = temp -> __rightlink;
+					//printf(" 2) The memory getting deallocated is: %p\n", temp -> memoryAddress);
+					if(temp == __memoryTableStart__){
+						__memoryTableStart__ = __memoryTableStart__ -> __rightlink;
+						__memoryTableStart__ -> __leftlink = NULL;
+					}
+					else if(temp == __memoryTableEnd__){
+						__memoryTableEnd__ = __memoryTableEnd__ -> __leftlink;
+						__memoryTableEnd__ -> __rightlink = NULL;
+					}
+					else{
+						prev = temp -> __leftlink;
+						prev -> __rightlink = temp -> __rightlink;
+					}
 					free(temp);
 					break;
 				}
 				temp = temp -> __rightlink;
 			}	
 		}
-		getMemoryStatus();
+		//getMemoryStatus();
+		//printf(" O - __memoryTableStart__ = %p\n", __memoryTableStart__);
+		//printf(" O - __memoryTableEnd__   = %p\n", __memoryTableEnd__);
 		return 0;
 	}
 }
-void *smalloc(char *dataType,unsigned int size)
+void __updateLinkedList(void *updateAddress,void *newMemoryAddress,unsigned int size)
+{
+	struct __memoryTable *temp;
+	temp = updateAddress;
+	temp -> memoryAddress = newMemoryAddress;
+	temp -> memorySize = size;
+}
+void *smalloc(unsigned int size)
 {
 	void *pointer = NULL;
 	pointer = malloc(size);
 	if(pointer!=NULL){
 		if(__pushLinkedList(pointer,size) == 1){
-			printf(" The push into the linked list was unsuccessful.\n");
+			//printf(" The push into the linked list was unsuccessful.\n");
 			free(pointer);
 		}
 	}
+	//printf(" The allocated memory location is: %p\n", pointer);
 	return pointer;
-	/*
-	if(strcmp(dataType,"int") == 0){
-		printf(" int dataType.\n");
-		pointer = malloc(size);
-		if(__pushLinkedList(pointer) == 0){
-			printf(" The push into the linked list was unsuccessful.\n");;
-		}
-		return pointer;
-	}
-	else if(strcmp(dataType,"char") == 0){
-		printf(" char dataType.\n");
-	}
-	else if(strcmp(dataType,"float") == 0){
-		printf(" float dataType.\n");
-	}
-	else if(strcmp(dataType,"double") == 0){
-		printf(" double dataType.\n");
-	}
-	else{
-		printf(" Not a dataType.\n");
+}
+void *srealloc(void *pointerAddress,unsigned int size) /* Returns NULL if an error occured*/
+{
+	struct __memoryTable *temp;
+	void *pointer;
+	unsigned int flag = 0;
+	if(pointerAddress == NULL){
+		//printf(" The memoryAddress is null, you first need to allocate memory.\n");
 		return NULL;
 	}
-	*/
+	else{
+		temp = __memoryTableStart__;
+		while(temp != NULL){
+			if(pointerAddress == temp -> memoryAddress){
+				//printf(" Yes it is present in the memory table.\n");
+				pointer = realloc(pointerAddress,size);
+				__updateLinkedList(temp,pointer,size);
+				flag = 1;
+				break;
+			}
+			temp = temp -> __rightlink;
+		}
+		if(flag == 0){
+			//printf(" Was not found in the memory table.\n");
+			return NULL;
+		}
+		else if(flag == 1){
+			return pointer;
+		}
+	}
 }
-int sfree(void *pointerAddress)
+unsigned int sfree(void *pointerAddress)
 {
 	if(pointerAddress!=NULL){
 		__popLinkedList(pointerAddress);
 		return 0;
 	}
 	else{
-		printf(" Pointer is NULL.\n");
+		//printf(" Pointer is NULL.\n");
 		return 0;
 	}
 }
-int freeAllMemeory()
+unsigned int freeAllMemeory()
 {
 	struct __memoryTable *temp;
 	if(__memoryTableStart__ == NULL){
-		printf(" No allocated memory.\n");
+		//printf(" No allocated memory.\n");
 		return 0;
 	}
 	else{
 		temp = __memoryTableStart__;
 		while(temp != NULL){
-			__popLinkedList(temp);
+			__popLinkedList(temp -> memoryAddress);
 			temp = temp -> __rightlink;
 		}
 		return 0;
 	}
 }
 #endif
-/* FOR TESTING PURPOSES */
-/*
-int main()
-{
-	int *charPointer;
-	charPointer = smalloc("int",sizeof(int));
-	getMemoryStatus();
-	sfree(charPointer);
-	freeAllMemeory();
-}
-*/
