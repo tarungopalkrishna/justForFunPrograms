a25831ae9b7bb2ffbee960a7fde28026358bd776
Tarun Gopalkrishna <tarungopalkrishna@gmail.com>
2019-07-26 09:45:05 +0530

    Added a tabular output and label for safeMemoryAllocation

--- safeMemoryAllocation/salloc.c
+++ safeMemoryAllocation/salloc.c
@@ -13,30 +13,36 @@
 struct __memoryTable
 {
 	//int 	memoryId;
-	//char 	*pointerName;
+	char 	*pointerName;
 	void 	*memoryAddress;
 	unsigned int 	memorySize;		//Think later if you want to make this pointer variable.
 	struct __memoryTable *__leftlink;
 	struct __memoryTable *__rightlink;
 };
 struct __memoryTable *__memoryTableStart__ = NULL,*__memoryTableEnd__ = NULL;
+
 // ALL THE FUNCTIONS
 /* Functions local to the program which must not be used in a user defined program */
-void *__getMemory(void *pointerAddress,unsigned int size);	//For the progam should not be used in your program.
-unsigned int __pushLinkedList(void *pointerAddress,unsigned int size);	//Push into the linked list.
+void *__getMemory(void *pointerAddress,unsigned int size,char *label);	//For the progam should not be used in your program.
+unsigned int __pushLinkedList(void *pointerAddress,unsigned int size,char *label);	//Push into the linked list.
 unsigned int __popLinkedList(void *pointerAddress);	//Pop from the linked list.
 void __updateLinkedList(void *updateAddress,void *newMemoryAddress,unsigned int size); // To update if memory is reallocated
 /* The functions that the programmer can use*/
 unsigned int getMemoryStatus();	//Gets the currrent allocated addresses.
-void *smalloc(unsigned int size);	//allocated memory and push into the linked list.
+unsigned int getMemoryStatusTable(); //Gets the currrent allocated addresses in a tabular form.
+void *smalloc(unsigned int size,char *label);	//allocated memory and push into the linked list.
+//void *smalloc(unsigned int size);	//allocated memory and push into the linked list.
+void *scalloc(unsigned int arraySize,unsigned int size,char *label);
 void *srealloc(void *pointerAddress,unsigned int size);		//reallocates a new piece of memory and updates __memoryTable
 unsigned int sfree(void *pointerAddress);	//free memory and pop it from the linked list.
 unsigned int freeAllMemory();	//free all the allocated memory
+
 // FUNCTION DEFINITIONS
-void *__getMemory(void *pointerAddress,unsigned int size)/* Allocated a node for __memoryTable*/
+void *__getMemory(void *pointerAddress,unsigned int size,char* label)/* Allocated a node for __memoryTable*/
 {
 	struct __memoryTable *temp;
 	temp = (struct __memoryTable*)malloc(sizeof(struct __memoryTable));
+	temp -> pointerName = strcpy(malloc(strlen(label)),label);
 	temp -> memoryAddress = pointerAddress;
 	temp -> memorySize = size;
 	temp -> __leftlink = NULL;
@@ -57,6 +63,7 @@ unsigned int getMemoryStatus()
 		while(temp != NULL){
 			count++;
 			printf(" Memory Address: %p\n", temp -> memoryAddress);
+			printf(" Memory lable  : %s\n", temp -> pointerName);
 			printf(" Memory size   : %d\n", temp -> memorySize	);
 			printf(" __leftlink    : %p\n", temp -> __leftlink);
 			printf(" __rightlink   : %p\n", temp -> __rightlink);
@@ -67,10 +74,40 @@ unsigned int getMemoryStatus()
 		return count;
 	}
 }
-unsigned int __pushLinkedList(void *pointerAddress,unsigned int size)
+unsigned int getMemoryStatusTable()
 {
+    unsigned int count = 0;
 	struct __memoryTable *temp;
-	temp = __getMemory(pointerAddress,size);
+	if(__memoryTableStart__ == NULL){
+		//printf(" No allocated memory.\n");
+		return 0;
+	}
+	else{
+        char seperator[] = "+------+--------------------------+----------------------+----------------+";        
+        char header[]    = "| SLNO |          LABEL           |    MEMROY ADDRESS    |      SIZE      |";
+
+        void printLine(int slno,char *label,void *memAddr,int size){
+            printf("+  %3d + %-25s+ %-20p + %14d +\n",slno,label,memAddr,size);
+        }
+
+        temp = __memoryTableStart__;
+        printf("%s\n",seperator);
+        printf("%s\n",header);
+        printf("%s\n",seperator);
+        while(temp != NULL){
+            count++;
+            printLine(count,temp->pointerName,temp->memoryAddress,temp->memorySize);
+            printf("%s\n",seperator);
+            temp = temp -> __rightlink;
+        }
+        return count;
+    }
+}
+
+
+unsigned int __pushLinkedList(void *pointerAddress,unsigned int size,char *label){
+	struct __memoryTable *temp;
+	temp = __getMemory(pointerAddress,size,label);
 	if(temp == NULL){
 		//printf(" Cannot push to linked list due to insufficient memory.\n");
 		return 1;
@@ -94,6 +131,11 @@ unsigned int __pushLinkedList(void *pointerAddress,unsigned int size)
 		return 0;
 	}
 }
+void free_node(struct __memoryTable *pointer)
+{
+	free(pointer->pointerName);
+	free(pointer);
+}
 unsigned int __popLinkedList(void *pointerAddress) // Code can be optimizes to return a proper value
 {
 	struct __memoryTable *temp,*prev;
@@ -107,7 +149,7 @@ unsigned int __popLinkedList(void *pointerAddress) // Code can be optimizes to r
 		temp = __memoryTableStart__;
 		if(__memoryTableStart__ == __memoryTableEnd__){
 			//printf(" 1) The memory getting deallocated is: %p\n", temp -> memoryAddress);
-			free(temp);
+			free_node(temp);
 			__memoryTableStart__ = NULL;
 			__memoryTableEnd__ = NULL;
 		}
@@ -127,7 +169,7 @@ unsigned int __popLinkedList(void *pointerAddress) // Code can be optimizes to r
 						prev = temp -> __leftlink;
 						prev -> __rightlink = temp -> __rightlink;
 					}
-					free(temp);
+					free_node(temp);
 					break;
 				}
 				temp = temp -> __rightlink;
@@ -146,32 +188,62 @@ void __updateLinkedList(void *updateAddress,void *newMemoryAddress,unsigned int 
 	temp -> memoryAddress = newMemoryAddress;
 	temp -> memorySize = size;
 }
-void *smalloc(unsigned int size)
+void *smalloc(unsigned int size,char *label)
 {
 	void *pointer = NULL;
 	pointer = malloc(size);
 	if(pointer!=NULL){
-		if(__pushLinkedList(pointer,size) == 1){
+		if(__pushLinkedList(pointer,size,label) == 1){
 			//printf(" The push into the linked list was unsuccessful.\n");
-			free(pointer);
+			free_node(pointer);
 		}
 	}
 	//printf(" The allocated memory location is: %p\n", pointer);
 	return pointer;
 }
-void *scalloc(unsigned int arraySize,unsigned int size)
+/*
+void *smalloc(unsigned int size)
+{
+	void *pointer = NULL;
+	pointer = malloc(size);
+	if(pointer!=NULL){
+		if(__pushLinkedList(pointer,size,"no_label") == 1){
+			//printf(" The push into the linked list was unsuccessful.\n");
+			free_node(pointer);
+		}
+	}
+	//printf(" The allocated memory location is: %p\n", pointer);
+	return pointer;
+}
+*/
+void *scalloc(unsigned int arraySize,unsigned int size,char *label)
 {
 	void *pointer = NULL;
 	pointer = calloc(arraySize,size);
 	if(pointer!=NULL){
-		if(__pushLinkedList(pointer,size) == 1){
+		if(__pushLinkedList(pointer,size,"no_label") == 1){
 			//printf(" The push into the linked list was unsuccessful.\n");
-			free(pointer);
+			free_node(pointer);
 		}
 	}
 	//printf(" The allocated memory location is: %p\n", pointer);
 	return pointer;	
 }
+/*
+void *scalloc(unsigned int arraySize,unsigned int size)
+{
+	void *pointer = NULL;
+	pointer = calloc(arraySize,size);
+	if(pointer!=NULL){
+		if(__pushLinkedList(pointer,size,"no_label") == 1){
+			//printf(" The push into the linked list was unsuccessful.\n");
+			free_node(pointer);
+		}
+	}
+	//printf(" The allocated memory location is: %p\n", pointer);
+	return pointer;	
+}
+*/
 void* srealloc(void *pointerAddress,unsigned int size) /* Returns NULL if an error occured*/
 {
 	struct __memoryTable *temp;
@@ -232,6 +304,7 @@ unsigned int freeAllMemory()
 }
 //The following function takes a number and the same number of varibales concatenates them and allocates 
 //new memory and returns the address. 
+/* 
 char *concat(int number,...)
 {
 	if(number > 0){
@@ -243,7 +316,7 @@ char *concat(int number,...)
 		size 			= size + strlen(stringParser);
 		returnString 	= (char*)smalloc(size);
 		strcpy(returnString,stringParser);
-		/* This part of the code is not tested for multile cases*/
+		// This part of the code is not tested for multile cases
 		while(count < number){
 			stringParser 	= va_arg(argument,char*);
 			size 			= size + strlen(stringParser);
@@ -251,7 +324,7 @@ char *concat(int number,...)
 			strcat(returnString,stringParser);
 			count++;
 		}
-		/* End of not tested code*/
+		// End of not tested code
 		va_end(argument);
 		return returnString;
 	}
@@ -264,4 +337,5 @@ char *concat(int number,...)
 		return NULL;
 	}
 }
+*/
 #endif
--- safeMemoryAllocation/salloc.h
+++ safeMemoryAllocation/salloc.h
@@ -6,9 +6,11 @@
 #ifdef __SAFE_ALLOC
 // ALL THE FUNCTIONS
 extern unsigned int getMemoryStatus();	//Gets the currrent allocated addresses.
-extern void *smalloc(unsigned int size);	//allocated memory and push into the linked list.
+unsigned int getMemoryStatusTable(); //Gets the currrent allocated addresses in a tabular form.
+extern void *smalloc(unsigned int size,char *label);	//allocated memory and push into the linked list.
+extern void *scalloc(unsigned int arraySize,unsigned int size,char *label);
 extern void *srealloc(void *pointerAddress,unsigned int size);		//reallocates a new piece of memory and updates __memoryTable
 extern unsigned int sfree(void *pointerAddress);	//free memory and pop it from the linked list.
 extern unsigned int freeAllMemory();	//free all the allocated memory
-extern char *concat(int number,...); //Concatenates the number of arguments are returns the addres of newly allocated memory.
+//extern char *concat(int number,...); //Concatenates the number of arguments are returns the addres of newly allocated memory.
 #endif
